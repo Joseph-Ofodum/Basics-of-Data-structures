98ec6d4df282b53caaecb8d959479844c41f3244
Ofodum Joseph <ofodumjoseph@yahoo.com>
2023-10-15 22:08:39 +0100

    first commit

---
+++ src/main/java/org/example/Array2D.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.lang.reflect.Array;
+
+public class Array2D {
+
+    int[][] arr = new int[4][4];
+    int count = 1;
+
+    public void array2D() {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = count;
+                count++;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    }
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        ArrayId arrayId= new ArrayId();
+//        arrayId.Array();
+
+        Array2D array2D= new Array2D();
+        array2D.array2D();
+    }
+}
